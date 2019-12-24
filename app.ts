fb42ff5f9ac537c1f866b49a9ad852c78a67f549
Steve Kudirka <skudirka@lakana.com>
2019-12-24 11:39:48 -0600

    Typed parameters.

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+const add = (n1:number, n2:number, showResult:boolean, phrase:string) => {
+    const answer = n1 + n2;
+    if( showResult ){
+        console.log(phrase + answer);
+    } else{
+        return answer;
+    }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is ';
+
+const result  = add(+number1, +number2, printResult, resultPhrase);
